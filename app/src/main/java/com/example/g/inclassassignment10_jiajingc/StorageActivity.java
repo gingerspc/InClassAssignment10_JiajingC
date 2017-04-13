c4f7c0ab7ae00a15b08c7587ba640c102f078254
Ginger <ginger.jiajingchen@gmail.com>
2017-04-12 20:40:14 -0400

    test4

--- app/src/main/java/com/example/g/inclassassignment10_jiajingc/StorageActivity.java
+++ app/src/main/java/com/example/g/inclassassignment10_jiajingc/StorageActivity.java
@@ -1,13 +1,66 @@
 package com.example.g.inclassassignment10_jiajingc;
 
+import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+
+import java.io.File;
+import java.io.IOException;
 
 public class StorageActivity extends AppCompatActivity {
+
+    private StorageReference mStorageRef;
+    private imageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_storage);
+
+        mStorageRef = FirebaseStorage.getInstance().getReference();
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+        File localFile = null;
+        try{
+            localFile = File.createTempFile("image", "jpg");
+
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+
+
+
+        Uri file = Uri.fromFile(new File("path/to/images/rivers.jpg"));
+        StorageReference riversRef = mStorageRef.child("images/rivers.jpg");
+
+        riversRef.putFile(file)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        // Get a URL to the uploaded content
+                        Picasso.with(StorageActivity.this)
+                                .load(localFile)
+                                .resize(imageView.getWidth(), imageView.getHeight())
+                                .centerInside()
+                                .into(imageView);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        // Handle unsuccessful uploads
+                        // ...
+                    }
+                });
     }
 }
